55b0b8a9dcfaf168ab33e407df76fcebc034e63d
Hossein Sadeghi <ho.mysterious@gmail.com>
2023-10-22 12:10:42 +0200

    fix: add animation to mobile social links

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -64,7 +64,7 @@ export function HomeScreen({ pad }: { pad: number }) {
         {description}
       </Text>
 
-      <Box display={{ base: "block", lg: "none" }}>
+      <Box display={{ base: "block", lg: "none" }} data-aos="zoom-out" data-aos-delay="1000">
         <Socials items={socials} />
       </Box>
 
